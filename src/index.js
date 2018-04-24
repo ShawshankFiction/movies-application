1de018ce9202a2c3c8e92605c8ab91576c8fb729
Thomas <trexmeyer@gmail.com>
2018-04-24 14:47:07 -0500

    now able to add movies

--- src/index.js
+++ src/index.js
@@ -2,21 +2,46 @@
  * es6 modules and imports
  */
 import $ from "jquery";
-import getMovies from "./api.js";
+import {getMovies, addMovie} from "./api.js";
+
 
 /**
  * require style imports
  */
 
-getMovies().then((movies) => {
+const movie = {id: 0, title: "", rating: 0, genre: ""};
 
+let addedMovie = Object.create(movie);
+
+var moviesArray = [];
+
+console.log(moviesArray);
+
+
+
+getMovies().then((movies) => {
+  let i = 0;
   $('#loading').css('display', 'none');
   $('#movieContainer').append("<ul>");
   movies.forEach(({title, rating, id}) => {
     $('#movieContainer').append(`<li>id#${id} - ${title} - rating: ${rating}</li>`);
+    moviesArray.push(movies[i]);
+    i++;
   });
   $('#movieContainer').append("</ul>");
 }).catch((error) => {
   alert('Oh no! Something went wrong.\nCheck the console for details.');
   console.log(error);
+});
+
+$('#submit-edit').click(function() {
+    addedMovie.title = $('#title-edit').val();
+    addedMovie.genre = $('#genre-edit').val();
+    addedMovie.rating = $('#rating-edit').val();
+    addedMovie.id = moviesArray.length + 1;
+    console.log(addedMovie);
+    moviesArray.push(addedMovie);
+    console.log(moviesArray);
+    addMovie(addedMovie);
+
 });
